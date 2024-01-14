69f2c6cc678d95eb38413ba9111b04a66d612bf4
ChayoBee <cacao34@hotmail.com>
2024-01-14 00:46:41 -0300

    Commit 2

--- Sistema-Adopcion/src/Components/MyCard.jsx
+++ Sistema-Adopcion/src/Components/MyCard.jsx
@@ -1,15 +1,19 @@
 import Card from 'react-bootstrap/Card';
+import Tags from './Tags';
 
-const MyCard = ({image, Name, Desc }) => {
+const MyCard = ({image, Name, Desc, Text, Color }) => {
     return (
         <>
-            <Card style={{width: '20px' }}>
+        <div div className='Cards'>
+            <Card style={{ width: '19rem' }}>
                 <Card.Img variant='top' src={image} className='Perro'/>
                 <Card.Body>
                     <Card.Title>{Name}</Card.Title>
                     {Desc}
                 </Card.Body>
+                <Tags text= {Text} color={Color}/>
             </Card>
+        </div>
         </>
     );
 };
--- Sistema-Adopcion/src/Components/Tags.jsx
+++ Sistema-Adopcion/src/Components/Tags.jsx
@@ -1,9 +1,12 @@
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 
 const Btn = (props) => {
     return (
         <>
-            <Button>{props.text}</Button>
+            <Button variant={props.color}>
+                {props.text} <Badge bg='secondary'></Badge>
+            </Button>
         </>
     );
 };
